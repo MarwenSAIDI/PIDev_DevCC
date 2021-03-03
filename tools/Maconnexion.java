603db57c22043d220cc50429d4af1384dbe77478
rachidchawech <47626619+rachidchawech@users.noreply.github.com>
2021-03-03 09:06:33 +0100

    Add files via upload

---
+++ tools/Maconnexion.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author aymen romdhani
+ */
+public class Maconnexion {
+    final static String URL ="jdbc:mysql://localhost:3306/crudproduit";
+   final static String LOGIN ="root";
+   final static String PWD ="";
+   static Maconnexion instance =null ;
+   private Connection cnx ;
+   
+   private Maconnexion (){
+       try{
+           cnx=DriverManager.getConnection(URL,LOGIN ,PWD);
+           System.out.println("connexion établie");
+       }catch (SQLException ex){
+           System.out.println("pas de connexion");
+       }
+       
+   }
+   public static Maconnexion getInstance(){
+       if (instance==null)
+       {
+           instance = new Maconnexion();
+       }
+       return instance;
+   }
+   public Connection getConnection(){
+       return cnx;
+   }
+    
+}
+
