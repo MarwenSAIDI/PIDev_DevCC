e4f7123d6c1476b75669a3bb3b101b775cf6b04e
Fourat888 <78850235+Fourat888@users.noreply.github.com>
2021-03-03 08:56:57 +0100

    Add files via upload

---
+++ tests/test.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TEST;
+
+
+
+/**
+ *
+ * @author foura
+ */
+import static javafx.application.Application.launch;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class test extends Application {
+    
+public static Stage stg;
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stg=stage;
+        Parent parent = (Parent) FXMLLoader.load(getClass().getResource(
+                "/projet/test.fxml"));
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+      
+                stage.setTitle("consultation");
+
+        stage.show();
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
