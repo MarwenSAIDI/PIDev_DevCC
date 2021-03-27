5e8ca4bbdbfd630140d7bcc8c38d4d4683a3edf9
rachidchawech <47626619+rachidchawech@users.noreply.github.com>
2021-03-27 21:38:38 +0100

    Add files via upload

--- entities/Produit.java
+++ entities/Produit.java
@@ -5,6 +5,8 @@
  */
 package Entities;
 
+import java.util.Objects;
+
 /**
  *
  * @author aymen romdhani
@@ -15,13 +17,16 @@ public class Produit {
     private String type;
     private int quantite;
     private float prix;
+    private String image;
 
-    public Produit(int ID, String nom, String type, int quantite, float prix) {
+    public Produit(int ID, String nom, String type, int quantite, float prix, String image) {
         this.ID =ID;
         this.nom =nom;
         this.type =type;
         this.quantite =quantite;
         this.prix =prix;
+        this.image=image;
+                
     }
 
     public Produit (){
@@ -69,12 +74,66 @@ public class Produit {
         this.prix = prix;
     }
 
-  
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 
     @Override
     public String toString() {
-        return "Produit{" + "ID=" + ID + ", nom=" + nom + ", type=" + type + ", quantite=" + quantite + ", prix=" + prix + "}\n";
+        return "Produit{" + "ID=" + ID + ", nom=" + nom + ", type=" + type + ", quantite=" + quantite + ", prix=" + prix + ", image=" + image + '}';
     }
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 47 * hash + this.ID;
+        hash = 47 * hash + Objects.hashCode(this.nom);
+        hash = 47 * hash + Objects.hashCode(this.type);
+        hash = 47 * hash + this.quantite;
+        hash = 47 * hash + Float.floatToIntBits(this.prix);
+        hash = 47 * hash + Objects.hashCode(this.image);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Produit other = (Produit) obj;
+        if (this.ID != other.ID) {
+            return false;
+        }
+        if (this.quantite != other.quantite) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.prix) != Float.floatToIntBits(other.prix)) {
+            return false;
+        }
+        if (!Objects.equals(this.nom, other.nom)) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        if (!Objects.equals(this.image, other.image)) {
+            return false;
+        }
+        return true;
+    }
+
+  
+
+   
     
 }
