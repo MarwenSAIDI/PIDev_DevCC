9b3ec76937a793b09e35d10a5857c845f1212f3a
yassinebenouaghrem <48158102+yassinebenouaghrem@users.noreply.github.com>
2021-03-30 17:08:44 +0100

    Add files via upload

--- therapeuteinterfaces/RecommandationController.java
+++ therapeuteinterfaces/RecommandationController.java
@@ -1,6 +1,3 @@
-
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -176,7 +173,7 @@ public class RecommandationController implements Initializable {
             alert.showAndWait();
         } 
            else{
-         Recommandation e=new Recommandation (textid.getText(),texttitre.getText(),textdescription.getText(),textecrivain.getText(),textimage.getText(),texttype.getText());
+         Recommandation e=new Recommandation (textid.getText(),texttitre.getText(),textdescription.getText(),textecrivain.getText(),textimage.getText(),texttype.getText(),"attente");
 Servicerecommandation s=new Servicerecommandation();
 s.addreco(e);
        Servicerecommandation xs= new Servicerecommandation();
