5ac70e971757bf1aaa90c31a0b5a5a8431da30b3
eskander1998 <47850204+eskander1998@users.noreply.github.com>
2021-04-13 01:35:30 +0100

    Add files via upload

---
+++ Form/ReservationEventType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ReservationEvent;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Evenement;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class ReservationEventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('idClient')
+            ->add('nbPlace', ChoiceType::class, [
+                'choices'  => [
+                    1 => 1,
+                    2 => 2,
+                    3 => 3,
+                    4 => 4,
+                    5 => 5,
+                ]])
+
+
+            ->add('modePaiement', ChoiceType::class, [
+                'choices'  => [
+                    "Sur place" => "Sur place",
+                    "En ligne" => "En ligne",
+                ] ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => ReservationEvent::class,
+        ]);
+    }
+
+}
